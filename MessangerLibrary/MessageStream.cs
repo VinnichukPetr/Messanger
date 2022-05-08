6fd6d05f6b275f19124bbf8cd2f6ee536779d02d
VinnichukPetr <vinnichukpetro@gmail.com>
2022-05-08 13:55:59 +0200

    Add MessageStream and data type <Message>, Enum colection <StatusMessage>

--- MessangerLibrary/MessageStream.cs
+++ MessangerLibrary/MessageStream.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.IO;
 
 namespace MessangerLibrary
 {
     public class MessageStream
     {
+        // create new Message
+        static public Message CreateMessage(StatusMessage typeMesage, string username, string content) => new Message()
+        {
+            TypeMessage = typeMesage,
+            Username = username,
+            Content = content
+        };
+
+        // transform message in bytes array and return it
+        static public byte[] WriteMessage(Message message)
+        {
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                using (BinaryWriter writer = new BinaryWriter(memoryStream))
+                {
+                    writer.Write((int)message.TypeMessage);
+                    writer.Write(message.Username);
+                    writer.Write(message.Content);
+                }
+                return memoryStream.ToArray();
+            }
+        }
+
+        // transform bytes array in message and return it
+        static public Message ReadMessage(byte[] byteMessage)
+        {
+            Message message = new Message();
+
+            using (MemoryStream memoryStream = new MemoryStream(byteMessage))
+            {
+                using (BinaryReader reader = new BinaryReader(memoryStream))
+                {
+                    message.TypeMessage = (StatusMessage)reader.ReadInt32();
+                    message.Username = reader.ReadString();
+                    message.Content = reader.ReadString();
+                }
+            }
+
+            return message;
+        }
     }
 }
