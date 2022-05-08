a7a5cdacc4253f1ae2ca04081ae790b533a7b8da
VinnichukPetr <vinnichukpetro@gmail.com>
2022-05-08 13:52:40 +0200

    Add project files.

---
+++ MessangerLibrary/Message.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessangerLibrary
+{
+    public enum StatusMessage
+    {
+        LogIn, //if log in messanger
+        SigIn, //if sig in messanger
+        Forgaut, // if forgaut password
+        ForgoutCheck, // if chek code for forgaut pasword
+        Joined, // if joined in chat
+        Message, // if sednd message
+        Detached, // if detached in chat
+        Null // null :)
+    }
+
+    public class Message
+    {
+        public StatusMessage TypeMessage { get; set; }
+        public string Username { get; set; }
+        public string Content { get; set; }
+
+        public override string ToString() => $"{Username}:\n{Content}";
+    }
+}
